7e6000485e09520b48a9b1246febfb093b309917
Russell Mills <russ@misfortu.net>
2016-05-31 20:18:23 +0000

    Now with JSON marshalling that works

--- client.go
+++ client.go
@@ -8,21 +8,35 @@ import (
 )
 
 type VenueStatus struct {
-	ok    bool
-	venue string
+	OK    bool `json:"ok"`
+	Venue string `json:"venue"`
 }
 
+type APIStatus struct {
+	OK    bool `json:"ok"`
+	Error string `json:"error"`
+}
+
+
 func main() {
+	// first call to Stockfighter API
 	endpoint := "https://api.stockfighter.io/ob/api/heartbeat"
 	bodytext := stockfighterAPI(endpoint)
 	fmt.Printf("Stockfighter heartbeat: %s\n", bodytext)
 
+	var apiStates APIStatus
+	err := json.Unmarshal(bodytext, &apiStates)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("API heartbeat: %+v\n", apiStates)
+	
 	venueHeartbeat := "https://api.stockfighter.io/ob/api/venues/TESTEX/heartbeat"
 	bodytext = stockfighterAPI(venueHeartbeat)
 	fmt.Printf("TESTEX heartbeat: %s\n", bodytext)
 
 	var venueStates VenueStatus
-	err := json.Unmarshal(bodytext, &venueStates)
+	err = json.Unmarshal(bodytext, &venueStates)
 	if err != nil {
 		panic(err)
 	}
